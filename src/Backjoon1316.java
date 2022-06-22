4142e714806876cee1fabd5ba10dd9c8d779a044
goseungwon <knmy0925@gmail.com>
2022-06-22 23:09:41 +0900

    1316 아직 못함

---
+++ src/Backjoon1316.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Backjoon1316 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int tc = s.nextInt();
+        String[] arr;
+        int count=0;
+
+        for (int i =0; i<tc; i++){
+            String word= s.next();
+            for (int j=0; j<word.length(); j++){
+                arr = new String[word.length()];
+                if (charAt=)
+                arr[j]= String.valueOf(word.charAt(j));
+            }
+        }
+    }
+}
