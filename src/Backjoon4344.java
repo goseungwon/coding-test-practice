b8586987b2d6baaf25ea44b5f359ba33367d3c87
goseungwon <knmy0925@gmail.com>
2022-06-22 17:59:59 +0900

    4344 부등호 방향때메..

--- src/Backjoon4344.java
+++ src/Backjoon4344.java
@@ -1,34 +1,33 @@
 import java.util.Scanner;
 
+
 public class Backjoon4344 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
         int n = s.nextInt();
-        int num;
 
-        double[] resultarr = new double[n];
-        int count=0;
+        double[] result = new double[n];
 
-        for (int i=0; i<n; n++){
-            num=s.nextInt();
-            int[] arr = new int[num];
-            int total=0;
-            for (int j=0; j<num; j++) {
-                arr[j]=s.nextInt();
-                total+=arr[j];
+         for (int i=0; i<n; i++) {
+            int num = s.nextInt();
+            int[] arr = new int[1001];
+            double total = 0;
+
+            for (int j = 0; j < num; j++) {
+                arr[j] = s.nextInt();
+                total += arr[j];
             }
-            for(int j=0; j<num; j++){
-                if(total/num>resultarr[j]) count++;
+
+            double count = 0;
+            total = total/num;
+
+            for (int k = 0; k < num; k++) {
+                if (total < arr[k]) count++; //부등호 방향 하..
             }
-            resultarr[i]=count/num;
-            System.out.println(resultarr[0]);
+            result[i]=(count/num)*100;
         }
-
-        for (int i=0; i<n; n++){
-            System.out.println(resultarr[i]*100 + "%");
-        }
-
-
+         for(int i=0; i<n; i++)
+             System.out.printf("%.3f%%\n",result[i]);
     }
 }
