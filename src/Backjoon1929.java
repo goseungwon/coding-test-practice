5e605bc9f41a0d1f79c4e1671790c8f8c785605f
goseungwon <knmy0925@gmail.com>
2022-06-24 14:26:43 +0900

    1929 시간초과

---
+++ src/Backjoon1929.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Backjoon1929 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int s = sc.nextInt();
+        int e = sc.nextInt();
+
+        for (int i=s; i<=e; i++){
+            int count=0;
+
+            for (int j=1; j<=i; j++){
+                if(i%j==0) count++;
+                if (count==2 && j==i)
+                    System.out.println(j);
+            }
+
+        }
+    }
+}
