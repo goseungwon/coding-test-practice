f8e0e25c54163b0add1503b8b5bf8f441865cd2d
goseungwon <knmy0925@gmail.com>
2022-06-24 11:47:47 +0900

    1316도 해결! 모두 if (arr[] =="문자")에서 if( arr[].equals("문자")) 로 변경하니까 됨

--- src/Backjoon1316.java
+++ src/Backjoon1316.java
@@ -5,33 +5,31 @@ public class Backjoon1316 {
         Scanner s = new Scanner(System.in);
 
         int tc = s.nextInt();
-        String[] arr;
         int count = 0;
 
         for (int i = 0; i < tc; i++) {
             String word = s.next();
             int len = word.length();
-            arr = new String[len];
+            String[] arr = new String[len];
             arr[0] = String.valueOf(word.charAt(0));
+
+            int index =0;
 
             Loop1 :
             for (int j = 1; j < len; j++) {
-                if (String.valueOf(word.charAt(j)) == String.valueOf(word.charAt(j-1))){
+
+                if (String.valueOf(word.charAt(j)).equals(arr[index])){
                     continue;
                 }
-                for (int k=0; k<j; k++){
-                    if (arr[k] == String.valueOf(word.charAt(j))){
+                for (int k=0; k<index; k++){
+                    if (arr[k].equals(String.valueOf(word.charAt(j)))){
                         count++;
                         break Loop1;
                     }
                 }
-
-                arr[j]=String.valueOf(word.charAt(j));
+                index++;
+                arr[index]=String.valueOf(word.charAt(j));
             }
-            System.out.println(arr[0]);
-            System.out.println(arr[1]);
-            System.out.println(arr[2]);
-
         }System.out.println(tc - count);
 
     }
