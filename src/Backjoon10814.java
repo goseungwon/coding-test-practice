1a7c3f281a437f01344049126ea35257ed8a4837
goseungwon <knmy0925@gmail.com>
2022-07-02 13:25:52 +0900

    10814 시간초과

---
+++ src/Backjoon10814.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Backjoon10814 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+//        int n = sc.nextInt();
+//        String[][] arr = new String[n][2];
+//
+//        for (int i=0; i<n; i++){
+//            arr[i][0]=sc.next();
+//            arr[i][1]=sc.next();
+//        }
+//
+//        for (int i=0; i<n; i++){
+//            for (int j=i+1; j<n; j++){
+//                if (Integer.parseInt(arr[i][0])>Integer.parseInt(arr[j][0])){
+//                    String s1 = arr[j][0];
+//                    arr[j][0] = arr[i][0];
+//                    arr[i][0] = s1;
+//
+//                    s1 = arr[j][1];
+//                    arr[j][1] = arr[i][1];
+//                    arr[i][1] = s1;
+//                }
+//            }
+//        }
+//
+//        for (int i=0; i<n; i++){
+//                bw.write(arr[i][0]);
+//                bw.write(arr[i][1]);
+//                bw.write("\n");
+//        }
+//        bw.flush();
+//        bw.close();
+
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st =new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(br.readLine());
+
+        String[][] arr = new String[n][2];
+
+        for (int i=0; i<n; i++){
+            for (int j=0; j<2; j++){
+                    arr[i][j] = st.nextToken();
+            }
+        }
+
+        for (int i=0; i<n; i++){
+            for (int j=i+1; j<n; j++){
+                if (Integer.parseInt(arr[i][0])<Integer.parseInt(arr[j][0])){
+                    String s1 = arr[j][0];
+                    arr[j][0] = arr[i][0];
+                    arr[i][0] = s1;
+
+                    String s2 = arr[j][1];
+                    arr[j][1] = arr[i][1];
+                    arr[i][1] = s2;
+                }
+            }
+        }
+
+        for (int i=0; i<n; i++){
+            for (int j=0; j<2; j++){
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
