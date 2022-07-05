ad6f1e50e0379277cf47bc6c2c46174451e02d61
goseungwon <knmy0925@gmail.com>
2022-07-05 22:04:04 +0900

    2108 반례를 못찾겠음

--- src/Backjoon2108.java
+++ src/Backjoon2108.java
@@ -11,8 +11,7 @@ public class Backjoon2108 {
         int choia = 0;
         int choib = 0;
         int result = 10000;
-        int resultb = 10000;
-
+        boolean bool = false;
 
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
@@ -22,39 +21,38 @@ public class Backjoon2108 {
 
         Arrays.sort(arr);
 
-        if (n == 1) {
+        if (n == 1)
             result = arr[0];
-        } else {
-            for (int i = 1; i < n; i++) {
-                if (arr[i] == arr[i - 1]) {
-                    choia++;
-                } else {
-                    if (choib < choia) {
-                        choib = choia;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] == arr[i - 1]) {
+                choia++;
+            }
+            if (arr[i] != arr[i - 1] || i == n) {
+                if (choib < choia) {
+                    choib = choia;
+                    result = arr[i];
+                    bool = false;
+                } else if (choia == choib && !bool) {
+                    if (arr[i] < result) {
                         result = arr[i];
-                    } else if (choia == choib) {
-                        choib = choia;
-                        if (result > arr[i]) {
-                            if (arr[i] < resultb) {
-                                result = resultb;
-                                resultb = arr[i];
-                            } else {
-                                result = arr[i];
-                            }
-                        }
-                        choia = 0;
+                        bool = true;
+                    }else {
+                        bool = true;
                     }
                 }
+                choia = 0;
             }
+        }
+
 
 
             bw.write(Math.round(count / n) + "\n");
             bw.write(arr[arr.length / 2] + "\n");
             bw.write(result + "\n");
-            bw.write(arr[n - 1] - arr[0]);
+            bw.write(arr[n-1] - arr[0] +"\n");
 
             bw.flush();
             bw.close();
-        }
     }
 }
