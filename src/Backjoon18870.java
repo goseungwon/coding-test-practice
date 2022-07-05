2a1f37374c1104d5f766d1ca18533fd384b85e3e
goseungwon <knmy0925@gmail.com>
2022-07-05 23:04:40 +0900

    18870 시간 초과

--- src/Backjoon18870.java
+++ src/Backjoon18870.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Backjoon18870 {
@@ -8,28 +9,65 @@ public class Backjoon18870 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int[] num;
-
-        boolean[] arr =new boolean[2000000001];
+        int[] num, num2;
 
         int n = Integer.parseInt(br.readLine());
 
         num=new int[n];
+        num2=new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i=0; i<n; i++){
             num[i]=Integer.parseInt(st.nextToken());
-            arr[num[i]+1000000000]=true;
+            num2[i]=num[i];
         }
 
-        for (int i=0; i<n; i++){
-            int count=0;
-            for (int j=0; j<num[i]+1000000000; j++){
-                if (arr[j])
-                    count++;
-                }
-            sb.append(count+" ");
+//-----------------------------------------------------
+//        for (int i=0; i<n; i++){
+//            int count=0;
+//            for (int j=0; j<n; j++) {
+//                if (num[i] > num[j]) {
+//                    count++;
+//                }
+//            }
+//                sb.append(count+" ");
+//        }
+
+        Arrays.sort(num2);
+
+        for (int i=1; i<num2.length; i++){
+            if (num2[i]==num2[i-1]){
+                num2[i-1]=1000000000;
+            }
         }
+        Arrays.sort(num2);
+
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                if (num[i]==num2[j]){
+                sb.append(j+" ");
+                break;
+                 }
+            }
+        }
+
+
+
+
+
+//---------------------------------------------------
+//
+//        for (int i=0; i<n; i++){
+//            int count=0;
+//            for (int j=0; j<n; j++){
+//                if (num[i]==num2[j]){
+//                sb.append(j-count+" ");
+//                break;
+//             }
+//                if (num2[j]==num2[j+1])
+//                    count++;
+//            }
+//        }
         System.out.print(sb);
     }
 }
