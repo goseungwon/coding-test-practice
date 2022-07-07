8e774e99b57be0f061bad1e59d6a66723e338ebb
goseungwon <knmy0925@gmail.com>
2022-07-07 23:05:03 +0900

    10815 성공

--- src/Backjoon10815.java
+++ src/Backjoon10815.java
@@ -1,47 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Backjoon10815 {
+    static int[] input;
+    static int n,m;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
-        int[] input, output;
-        int n,m=0;
 
-        n= Integer.parseInt(st.nextToken());
+        n= Integer.parseInt(br.readLine());
         input=new int[n];
 
+        String a = br.readLine();
+        StringTokenizer st = new StringTokenizer(a);
 
-        for (int i=0; i<n; i++){
-            if (st.hasMoreTokens())
-                input[i]= Integer.parseInt(st.nextToken());
-            System.out.println(input[i]);
+        int idx = 0;
+        while (st.hasMoreTokens()) {
+            input[idx] = Integer.parseInt(st.nextToken());
+            idx++;
         }
 
-        if (st.hasMoreTokens()) {
-            m = Integer.parseInt(st.nextToken());
-        }
-        output=new int[m];
+        Arrays.sort(input);
 
-        for (int j=0; j<m; j++){
-            if (st.hasMoreTokens())
-                output[j]= Integer.parseInt(st.nextToken());
-            System.out.println(output[j]);
-        }
+        m = Integer.parseInt(br.readLine());
+        st=new StringTokenizer(br.readLine());
 
         for (int i=0; i<m; i++){
-            for (int j=0; j<n; j++){
-                if (output[i]==input[j]){
-                    sb.append(1+" ");
-                    continue;
-                }
-            }sb.append(0+" ");
+            int num= Integer.parseInt(st.nextToken());
+            if (binary(num)){
+                sb.append(1+" ");
+            }else
+                sb.append(0+" ");
         }
-        System.out.println(sb);
 
+        System.out.println(sb);
+    }
+
+    private static boolean binary(int num){
+        int left=0;
+        int right=n-1;
+
+        while  (left<=right){
+            int midindex=(left+right)/2;
+            int mid=input[midindex];
+
+            if (num<mid)
+                right=midindex-1;
+            else if (num>mid)
+                left=midindex+1;
+            else return true;
+        }
+        return false;
     }
 }
