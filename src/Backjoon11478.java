bd4787ce622e014dec7d1e73c575791819b45d61
goseungwon <knmy0925@gmail.com>
2022-07-16 23:46:43 +0900

    11478 하는중

---
+++ src/Backjoon11478.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Backjoon11478 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count=0;
+
+        String input = br.readLine();
+
+        String[] arr = input.split("");
+
+        for (int i=0; i< arr.length; i++){
+
+        }
+    }
+}
