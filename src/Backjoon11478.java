f7997855c20cc7debe188b952875dc62f9ccf7c5
goseungwon <knmy0925@gmail.com>
2022-07-17 22:46:47 +0900

    11478 정답

--- src/Backjoon11478.java
+++ src/Backjoon11478.java
@@ -1,12 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Backjoon11478 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder st;
 
+        Map map = new HashMap();
         int count=0;
 
         String input = br.readLine();
@@ -14,7 +17,16 @@ public class Backjoon11478 {
         String[] arr = input.split("");
 
         for (int i=0; i< arr.length; i++){
-
+            st = new StringBuilder();
+            for (int j=0; j<arr.length-i; j++){
+                st.append(arr[i+j]);
+                String in = String.valueOf(st);
+                if (!map.containsKey(in)){
+                    map.put(in, 0);
+                    count++;
+                }
+            }
         }
+        System.out.println(count);
     }
 }
