c688eecbd1087309fee11f6c3fb54dabed011520
goseungwon <knmy0925@gmail.com>
2022-07-22 10:34:25 +0900

    1037 성공

---
+++ src/basic/math/Backjoon1037.java
@@ -0,0 +1,27 @@
+package basic.math;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Backjoon1037 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i=0; i<n; i++){
+            arr[i]= Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        if (n==1) System.out.println(arr[0]*arr[0]);
+        else
+        System.out.println(arr[0]* arr[arr.length-1]);
+    }
+}
