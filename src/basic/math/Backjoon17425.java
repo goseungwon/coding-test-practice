49f20217b8b40ab30dd7867132774df939561106
goseungwon <knmy0925@gmail.com>
2022-07-25 10:02:20 +0900

    17425 모르겠다 다음에..

--- src/basic/math/Backjoon17425.java
+++ src/basic/math/Backjoon17425.java
@@ -5,34 +5,47 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Backjoon17425 {
+
+    /*
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int num= Integer.parseInt(br.readLine());
-        int[] arr = new int[10001];
-        int max=0;
 
         for (int j=0; j<num; j++){
             int input=Integer.parseInt(br.readLine());
-
-            if (max<input)
-                max=input;
-
-             arr[input]=0;
+            sb.append(getOutput(input)+"\n");
         }
-        getOutput(max,arr);
 
         System.out.println(sb);
     }
 
-    private static void getOutput(int max, int[] arr) {
-        long output=0;
-        for (int i = 1; i< Math.sqrt(max); i++){
-            output +=i*(max /i);
-            if (map.containsKey(i)){            //약수부터 올라가보자자
-              sb.append(output+"\n");
-            }
+    private static long getOutput(int input) {
+        long output=input*(input+1)/2;
+        for (int i = 1; i<=input/2; i++){
+            output +=i*(input /i);
+        }
+        return output;
+    }
+     */
+
+    public static void main(String[] args) {
+        int arr[] = new int[1000001];
+        arr[1]=1;
+        for (int i=2; i<arr.length; i++){
+            arr[i]+=arr[i-1]+getOutput(i);
         }
     }
+
+    private static long getOutput(int i) {
+        long output=0;
+        for (int j=0; j<Math.sqrt(i); j++){
+
+        }
+        return 0;
+
+    }
+
+
 }
