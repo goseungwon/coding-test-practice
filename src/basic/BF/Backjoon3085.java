740a98ac0798306fcbcd81a95282e0c05024c835
goseungwon <knmy0925@gmail.com>
2022-07-27 15:56:59 +0900

    3085 정답 // 문제꼼곰히읽기 // 오타조심

--- src/basic/BF/Backjoon3085.java
+++ src/basic/BF/Backjoon3085.java
@@ -3,65 +3,73 @@ package basic.BF;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Backjoon3085 {
+    static char[][] arr;
+    static int max=0;
+    static int n;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n= Integer.parseInt(br.readLine());
-        char [][] arr = new char[n][n];
-        int max=0, maxLine;
-        boolean maxHang;
-        boolean hang=false;
-        int y=0, c=0, p=0,z=0;
+        n= Integer.parseInt(br.readLine());
+        arr = new char[n][n];
 
-        for (int i=0; i<arr.length; i++){
+        for (int i=0; i<n; i++){
             String input=br.readLine();
-            for (int j=0; j<arr.length; j++){
+            for (int j=0; j<input.length(); j++){
                 arr[i][j]=input.charAt(j);
             }
         }
+        //배열에 입력
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n-1;j++) {
+                swap(i,j);
+                getMax();
+                getMax2();
+                swap(i,j);
 
-        for (int i=0; i<arr.length; i++){
-            y=0; c=0; p=0; z=0;
-            for (int j=0; j<arr.length; j++){
-                    switch (arr[i][j]){
-                        case 'Y' : y++; break;
-                        case 'C' : c++; break;
-                        case 'P' : p++; break;
-                        case 'Z' : z++;
-                }
-            }int temp = Math.max(Math.max(y, c), Math.max(p, z));
-            if (max<= temp){
-                max= temp;
-                maxLine=i;
-                maxHang=hang;
+                swap2(i,j);
+                getMax();
+                getMax2();
+                swap2(i,j);
             }
         }
-
-        for (int i=0; i<arr.length; i++){
-            y=0; c=0; p=0; z=0;
-            for (int j=0; j<arr.length; j++){
-                    switch (arr[j][i]){
-                        case 'Y' : y++; break;
-                        case 'C' : c++; break;
-                        case 'P' : p++; break;
-                        case 'Z' : z++;
-                }
-            }int temp = Math.max(Math.max(y, c), Math.max(p, z));
-            if (max<= temp){
-                max= temp;
-                maxLine=i;
-                maxHang=hang;
-            }
-        }
-
         System.out.println(max);
-
-
     }
-    private static int isPlus(int[][] arr){
 
+
+    private static void swap(int i, int j){
+        char swap = arr[i][j];
+        arr[i][j] = arr[i][j + 1];
+        arr[i][j + 1] = swap;
     }
-    private static int getMax(int[][] arr, )
+    private static void swap2(int i, int j){
+        char swap = arr[j][i];
+        arr[j][i] = arr[j+1][i];
+        arr[j+1][i] = swap;
+    }
+
+    private static void getMax() {
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j][i] == arr[j + 1][i]) count++;
+                else count = 1;
+                max = Math.max(count, max);
+            }
+
+        }
+    }
+
+    private static void getMax2(){
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[i][j] == arr[i][j + 1]) count++;
+                else count = 1;
+                max = Math.max(max, count);
+            }
+        }
+    }
 }
