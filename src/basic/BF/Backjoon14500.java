cac76d84a8d0cdbf9191ad2fe84dbb5ecc70ea6b
goseungwon <knmy0925@gmail.com>
2022-08-02 15:21:42 +0900

    14500 틀림 / 트리로 풀어보자

--- src/basic/BF/Backjoon14500.java
+++ src/basic/BF/Backjoon14500.java
@@ -3,20 +3,21 @@ package basic.BF;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Backjoon14500 {
     static int[][] arr;
-    static int count=0;
+    static int sum; // 총합
+    static int x,y;  //입력값
+    static int count; // 횟수
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
 
-        int y = Integer.parseInt(st.nextToken());
-        int x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
         arr = new int[y][x];
 
 
@@ -32,16 +33,58 @@ public class Backjoon14500 {
         //시작 위치
         for (int i=0; i<y; i++){
             for (int j=0; j<x; j++){
-                //탐색 메서드
+                count =0;
+                moveEverywhere(i,j, sum);
             }
         }
 
+        System.out.println(sum);
+    }
 
-        System.out.println(count);
 
+
+    public static void moveEverywhere(int startY, int startX, int sum){
+        while(count<4) {
+            moveToRight(startY, startX, sum);
+            moveToLeft(startY, startX, sum);
+            moveToTop(startY, startX, sum);
+            moveToBottom(startY, startX, sum);
+            count++;
+        }
     }
 
 
     //탐색 메서드
-    //public void
+    public static int moveToRight(int startY, int startX, int sum){
+        startX++;
+        if (startX==x) return 0;
+        sum+=arr[startY][startX];
+        moveEverywhere(startY,startX,count);
+        return sum;
+    }
+
+    public static int moveToLeft(int startY, int startX, int sum){
+        startX--;
+        if (startX<0) return 0;
+        sum+=arr[startY][startX];
+        moveEverywhere(startY,startX,count);
+        return sum;
+    }
+
+    public static int moveToTop(int startY, int startX, int sum){
+        startY++;
+        if (startY==y) return 0;
+        sum+=arr[startY][startX];
+        moveEverywhere(startY,startX,count);
+        return sum;
+    }
+
+    public static int moveToBottom(int startY, int startX, int sum){
+        startY--;
+        if (startY<0) return 0;
+        sum+=arr[startY][startX];
+        moveEverywhere(startY,startX,count);
+        return sum;
+    }
+
 }
