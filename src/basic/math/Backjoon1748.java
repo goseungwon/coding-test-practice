6d53e03c45cc0d20e96641528538a3de67f4a227
goseungwon <knmy0925@gmail.com>
2022-08-12 16:21:11 +0900

    1748 하는중

---
+++ src/basic/math/Backjoon1748.java
@@ -0,0 +1,23 @@
+package basic.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Backjoon1748 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n= Integer.parseInt(br.readLine());
+        int count=0;
+
+        for (int i=8; i>=0; i--){
+            System.out.println(Math.pow(10,i));
+            count= count + (n/(int) Math.pow(10,i))*i;
+            n%=(int) Math.pow(10,i);
+            System.out.println("count = " + count);
+            System.out.println("n = " + n);
+        }
+        System.out.println(count);
+    }
+}
