d0f2977bf7e57b0e103ca6b11f25f7ef40c4b90c
goseungwon <knmy0925@gmail.com>
2022-08-17 15:45:58 +0900

    17425 완료 / 블로그 참고

--- src/basic/math/Backjoon17425.java
+++ src/basic/math/Backjoon17425.java
@@ -3,49 +3,34 @@ package basic.math;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Backjoon17425 {
-
-    /*
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int num= Integer.parseInt(br.readLine());
+        long arrfx[] = new long[1000001];
+        long arrgx[] = new long[1000001];
+        Arrays.fill(arrfx,1);
+        arrgx[1]=1;
 
-        for (int j=0; j<num; j++){
-            int input=Integer.parseInt(br.readLine());
-            sb.append(getOutput(input)+"\n");
+        for (int i=2; i<arrfx.length; i++){
+            for (int j = 1; j*i <arrfx.length; j++) {
+                arrfx[i*j]+=i;
+            }
+            arrgx[i] = arrgx[i-1] + arrfx[i];
         }
 
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < tc; i++) {
+            int input = Integer.parseInt(br.readLine());
+            sb.append(arrgx[input] +"\n");
+        }
         System.out.println(sb);
     }
 
-    private static long getOutput(int input) {
-        long output=input*(input+1)/2;
-        for (int i = 1; i<=input/2; i++){
-            output +=i*(input /i);
-        }
-        return output;
-    }
-     */
-
-    public static void main(String[] args) {
-        int arr[] = new int[1000001];
-        arr[1]=1;
-        for (int i=2; i<arr.length; i++){
-            arr[i]+=arr[i-1]+getOutput(i);
-        }
-    }
-
-    private static long getOutput(int i) {
-        long output=0;
-        for (int j=0; j<Math.sqrt(i); j++){
-
-        }
-        return 0;
-
-    }
 
 
 }
