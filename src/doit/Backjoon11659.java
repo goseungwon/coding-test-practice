442aef0275296f484c47abf318efd14baefe1d99
goseungwon <knmy0925@gmail.com>
2022-09-13 10:42:41 +0900

    11659 효율개선

--- src/doit/Backjoon11659.java
+++ src/doit/Backjoon11659.java
@@ -14,24 +14,23 @@ public class Backjoon11659 {
         int arrLen = Integer.parseInt(st.nextToken());
         int tc = Integer.parseInt(st.nextToken());
 
-        int[] input = new int[arrLen];
-        long[] total = new long[arrLen];
+        int[] input = new int[arrLen+1];
+        long[] total = new long[arrLen+1];
+        input[0]=0;
+        total[0]=0;
 
         st = new StringTokenizer(br.readLine());
-        for (int i=0; i<arrLen; i++){
+        for (int i=1; i<=arrLen; i++){
             input[i] = Integer.parseInt(st.nextToken());
-            if (i==0)  total[i] = input[i];
-            else total[i] = total[i-1] + input[i];
-//            System.out.println("input[i] = " + input[i]);
-//            System.out.println("total[i] = " + total[i]);
+            total[i] = total[i-1] + input[i];
         }
 
         for (int i=0; i<tc; i++){
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken())-1;
-            int end = Integer.parseInt(st.nextToken())-1;
+            int end = Integer.parseInt(st.nextToken());
 
-            sb.append(total[end] - total[start] + input[start] + "\n");
+            sb.append(total[end] - total[start] + "\n");
         }
 
         System.out.println(sb);
