cfb43072092579dff8f2af37342677a029457640
goseungwon <knmy0925@gmail.com>
2022-09-15 20:12:52 +0900

    1717 하는중

---
+++ src/doit/Backjoon1717.java
@@ -0,0 +1,19 @@
+package doit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Backjoon1717 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num = Integer.parseInt(st.nextToken());
+        int tc = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer>
+    }
+}
