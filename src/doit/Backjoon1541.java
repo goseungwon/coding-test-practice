cf451289b7c74436540c64045c7e98e9d9a3a162
goseungwon <knmy0925@gmail.com>
2022-09-15 19:45:13 +0900

    1541 성공

---
+++ src/doit/Backjoon1541.java
@@ -0,0 +1,34 @@
+package doit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Backjoon1541 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int result = 0;
+
+        String input = br.readLine();
+        String[] inputArr = input.split("-");
+
+        for (int i=0; i<inputArr.length; i++){
+            if (i==0) result += sum(inputArr[i]);
+            else result -= sum(inputArr[i]);
+        }
+        System.out.println(result);
+
+    }
+
+    static int sum(String a){
+        int sum = 0;
+        String[] temp = a.split("\\+");
+        for (int i=0; i<temp.length; i++){
+            sum += Integer.parseInt(temp[i]);
+        }
+        return sum;
+    }
+}
