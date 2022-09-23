353ad434a3390a8db7566f052b0b0858742a0fed
goseungwon <knmy0925@gmail.com>
2022-09-23 11:42:48 +0900

    11654 정답

---
+++ src/sequence/No6/Backjoon11654.java
@@ -0,0 +1,13 @@
+package sequence.No6;
+
+import java.util.Scanner;
+
+public class Backjoon11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.next().charAt(0);
+
+        System.out.println(a);
+    }
+}
