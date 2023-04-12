6f89887bded8f3099573168473de0fc2c074b629
goseungwon <knmy0925@gmail.com>
2023-04-12 15:58:32 +0900

    lv.2 연속 부분 수열 합의 개수 성공

---
+++ src/programmers/programmerstest/연속_부분_수열_합의_개수.java
@@ -0,0 +1,19 @@
+package programmers.programmerstest;
+
+import java.util.*;
+
+public class 연속_부분_수열_합의_개수 {
+	public int solution(int[] elements) {
+		Set<Integer> set = new HashSet<>();
+		int length = elements.length;
+
+		for (int front=0; front<length; front++) {
+			int count = 0;
+			for (int length2=0; length2<elements.length; length2++) {
+				count+=elements[(front+length2)%length];
+				set.add(count);
+			}
+		}
+		return set.size();
+	}
+}
