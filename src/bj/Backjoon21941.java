f86e1d32b94852e541f5df7cbcbb20f8c87d73de
goseungwon <knmy0925@gmail.com>
2023-04-24 00:14:54 +0900

    backjoon 21941 성공 / dp 공부하자

--- src/bj/Backjoon21941.java
+++ src/bj/Backjoon21941.java
@@ -3,17 +3,16 @@ package bj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Backjoon21941 {
 	static class Node{
-		double efficiency;
 		int score;
 		String str;
 
-		public Node(double efficiency, int score, String str) {
-			this.efficiency = efficiency;
+		public Node(int score, String str) {
 			this.score = score;
 			this.str = str;
 		}
@@ -21,33 +20,27 @@ public class Backjoon21941 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		StringBuilder input = new StringBuilder();
-		PriorityQueue<Node> queue = new PriorityQueue<>((o1, o2) -> Double.compare(o2.efficiency, o1.efficiency));
+		List<Node> list = new ArrayList<>();
 
-
-		input.append(br.readLine());
+		String input = br.readLine();
 		int length = Integer.parseInt(br.readLine());
 
-		int answer = 0;
 		for (int i=0; i<length; i++) {
 			st = new StringTokenizer(br.readLine());
 			String tempStr = st.nextToken();
 			int tempScore = Integer.parseInt(st.nextToken());
-			if (tempStr.length() >= tempScore) continue;
-			queue.add(new Node((double)tempStr.length() / tempScore, tempScore, tempStr));
+			list.add(new Node(tempScore, tempStr));
 		}
 
-			while(!queue.isEmpty()) {
-				Node curr = queue.poll();
-				while(true) {
-					int temp = input.indexOf(curr.str);
-					if (temp==-1) break;
-					answer+= curr.score - 1;
-					input.delete(temp, temp + curr.str.length());
-					input.insert(temp, "0");
-				}
+		int[] dp = new int[input.length()+1];
+		for (int i=0; i<input.length(); i++){
+			dp[i+1] = Math.max(dp[i+1], dp[i] + 1);
+			for (int j=0; j<length; j++) {
+				Node curr = list.get(j);
+				if (input.startsWith(curr.str, i)) dp[i + curr.str.length()] = Math.max(dp[i + curr.str.length()], dp[i] + curr.score);
 			}
+		}
 
-		System.out.println(answer + input.length());
+		System.out.println(dp[input.length()]);
 	}
 }
