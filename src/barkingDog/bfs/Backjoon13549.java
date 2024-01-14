c3faec26804c327b80ba27c22f193d54df4b52c9
goseungwon <knmy0925@gmail.com>
2024-01-15 00:30:04 +0900

    Backjoon13549 정답

--- src/barkingDog/bfs/Backjoon13549.java
+++ src/barkingDog/bfs/Backjoon13549.java
@@ -1,10 +1,8 @@
 package barkingDog.bfs;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -12,10 +10,8 @@ import java.util.StringTokenizer;
 public class Backjoon13549 {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static StringTokenizer st;
-	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	static int[] map = new int[100_001];
-	static boolean[] isVisited = new boolean[100_001];
+	static boolean[] isVisited = new boolean[200_001];
 	static int results = Integer.MAX_VALUE;
 	static int subin;
 	static int sister;
@@ -36,27 +32,24 @@ public class Backjoon13549 {
 	private static void solve() {
 		Queue<Node> queue = new LinkedList<>();
 		queue.add(new Node(subin, 0));
-		isVisited[subin] = true;
 
 		while (!queue.isEmpty()) {
 			Node node = queue.remove();
+			isVisited[node.index] = true;
 
 			if (node.index == sister){
-				results = node.time;
-				return;
+				results = Math.min(node.time, results);
+				continue;
 			}
 
 			if(isValid(node.index*2)) {
 				queue.add(new Node(node.index*2, node.time));
-				isVisited[node.index*2] = true;
 			}
 			if(isValid(node.index+1)) {
 				queue.add(new Node(node.index+1, node.time+1));
-				isVisited[node.index+1] = true;
 			}
 			if(isValid(node.index-1)) {
 				queue.add(new Node(node.index-1, node.time+1));
-				isVisited[node.index-1] = true;
 			}
 		}
 
@@ -67,7 +60,7 @@ public class Backjoon13549 {
 	}
 
 	private static boolean isValid(int index) {
-		return index>=0 && index<=100_000 && !isVisited[index];
+		return index>=0 && index<=100_001 && !isVisited[index];
 	}
 
 	static class Node {
